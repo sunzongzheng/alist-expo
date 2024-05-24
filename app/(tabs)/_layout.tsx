1b92aaf2a1a743ec37afe0a64ad09d6f4b165173
sunzongzheng <sunzongzhengwy@163.com>
2024-05-24 11:53:02 +0800

    feat: 解决覆盖安装后无法启动服务的问题

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
@@ -7,7 +7,7 @@ import {appendLog} from "@/app/store/log";
 import {NativeEventEmitter, NativeModules} from "react-native";
 import {useAppDispatch} from "@/app/store";
 import {refreshIsRunning} from "@/app/store/server";
-// import RNFS from 'react-native-fs'
+import RNFS from 'react-native-fs'
 
 const {Alist} = NativeModules;
 const eventEmitter = new NativeEventEmitter(Alist);
@@ -16,6 +16,27 @@ export default function TabLayout() {
   // const colorScheme = useColorScheme();
   const colorScheme = 'light';
   const dispatch = useAppDispatch();
+  const ensureConfigDirectory = useCallback(async () => {
+    /*
+    背景：
+    1. ios覆盖安装应用时，会创建一个新的Document目录，同时会把旧文件拷贝过去
+    2. config文件中存储的日志文件、临时目录等路径都是绝对路径
+
+    问题：由于Document目录已更新，但是config文件中存储的文件路径没有更新，服务启动后仍向旧的Document目录读写文件，会导致读写无权限
+
+    解法：这里对config文件中存储的文件路径进行处理，替换为新的Document目录
+     */
+    try {
+      const configData = await RNFS.readFile(RNFS.DocumentDirectoryPath + `/alist/config.json`)
+      if (configData.includes(RNFS.DocumentDirectoryPath)) return
+      let patternString = RNFS.DocumentDirectoryPath.replace(/\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\//, '/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/')
+      const regexPattern = new RegExp(patternString, 'g');
+      const newConfigData = configData.replace(regexPattern, RNFS.DocumentDirectoryPath)
+      await RNFS.writeFile(RNFS.DocumentDirectoryPath + `/alist/config.json`, newConfigData)
+    } catch (e) {
+      console.error(e)
+    }
+  }, [])
 
   useEffect(() => {
     const onLog = eventEmitter.addListener('onLog', (logInfo) => {
@@ -42,15 +63,9 @@ export default function TabLayout() {
     }
   }, [])
 
-  // console.log('RNFS.DocumentDirectoryPath', RNFS.DocumentDirectoryPath)
-  // RNFS.readDir(RNFS.DocumentDirectoryPath + '/alist')
-  //     .then((result) => {
-  //       console.log('GOT RESULT', result.map(item => item.name));
-  //     })
-  // RNFS.readFile(RNFS.DocumentDirectoryPath + `/alist/config.json`)
-  //     .then((result) => {
-  //       console.log(result);
-  //     })
+  useEffect(() => {
+    ensureConfigDirectory()
+  }, [])
 
   return (
     <Tabs
